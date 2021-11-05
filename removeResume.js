d811784a14301dc42a8457721fee60ae987ca637
datalater <the7mincheol@gmail.com>
2021-11-05 10:29:08 +0900

    feat: reposition modal to show user name in attendance
    - Add margin top to attendance modals
    - Fix manifest unknown field error
    - Format by prettier

--- removeResume.js
+++ removeResume.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-console */
 export default function removeResume() {
-  const RESUME_ID = 'resume-lesson-video';
+  const RESUME_ID = "resume-lesson-video";
 
   const $resume = document.getElementById(RESUME_ID);
 
   if (!$resume) {
     // eslint-disable-next-line no-restricted-globals
-    console.log('there is no resume');
+    console.log("there is no resume");
     return;
   }
 
-  Object.assign($resume.style, { display: 'none' });
+  Object.assign($resume.style, { display: "none" });
 }
--- removeScroll.js
+++ removeScroll.js
@@ -1,14 +1,24 @@
 /* eslint-disable no-console */
 export default function removeScroll() {
-  const ATTENDANCE_MODAL_SELECTOR = '.__vuescroll .table-wrap';
+  const addMarginTopToModals = (margin = 200) => {
+    const modalDialogs = document.querySelectorAll(".modal-dialog");
+
+    Array.from(modalDialogs).forEach((modal) => {
+      modal.style.marginTop = `${margin}px`;
+    });
+  };
+
+  addMarginTopToModals(200);
+
+  const ATTENDANCE_MODAL_SELECTOR = ".__vuescroll .table-wrap";
 
   const attendance = document.querySelector(ATTENDANCE_MODAL_SELECTOR);
 
   if (!attendance) {
     // eslint-disable-next-line no-restricted-globals
-    console.log('there is no attendance modal');
+    console.log("there is no attendance modal");
     return;
   }
 
-  Object.assign(attendance.style, { 'max-height': '100%' });
+  Object.assign(attendance.style, { "max-height": "100%" });
 }
